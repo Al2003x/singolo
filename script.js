c3dc24ea752728fb08663c9a97dbe0a01d89ce3a
Al2003x <Danilovvik@bk.ru>
2020-03-16 00:31:17 +0300

    add: js portfolio

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ nav.addEventListener("click", event => {
     .forEach(el => el.classList.remove("main-nav__link--active"));
   event.target.classList.add("main-nav__link--active");
 });
+
+const portfolioMenu = document.querySelector(".portfolio-breadcrumbs__list");
+
+portfolioMenu.addEventListener("click", event => {
+  event.preventDefault();
+  portfolioMenu
+    .querySelectorAll(".portfolio-breadcrumbs__link")
+    .forEach(el => el.classList.remove("portfolio-breadcrumbs__link--active"));
+  event.target.classList.add("portfolio-breadcrumbs__link--active");
+});
+
+const images = document.querySelector(".catalog-images");
+
+images.addEventListener("click", event => {
+  event.preventDefault();
+  images
+    .querySelectorAll(".catalog-images__img img")
+    .forEach(el => el.classList.remove("portfolio-image"));
+  event.target.classList.add("portfolio-image");
+});
+
+portfolioMenu.addEventListener("click", function() {
+  for (var i = images.children.length; i >= 0; i--) {
+    if (event.target.classList.contains("portfolio-breadcrumbs__link")) {
+      images.appendChild(images.children[(Math.random() * i) | 0]);
+    }
+  }
+});
